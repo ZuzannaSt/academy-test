d4b4fdd96f591fe8f0a8da92d50b16c91bf9fcd8
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-02-27 10:37:24 +0100

    added age column to User model

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :age, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :age, :current_password, :password, :password_confirmation) }
+  end
+end
