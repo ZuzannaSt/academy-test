91fe44c664f212007bab19bdcd2ba89d7ddd9bba
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-02-27 19:49:08 +0100

    added few cities to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+
+city1 = City.find_or_create_by(name: 'New York', short_name: 'NY')
+city2 = City.find_or_create_by(name: 'Paris', short_name: 'PAR')
+city3 = City.find_or_create_by(name: 'London', short_name: 'LON')
+city4 = City.find_or_create_by(name: 'Warszawa', short_name: 'WAW')
+city5 = City.find_or_create_by(name: 'Kraków', short_name: 'KRK')
+city6 = City.find_or_create_by(name: 'Wrocław', short_name: 'WRO')
+puts 'FOUND OR CREATED CITIES: ' << city1.name, city2.name, city3.name, city4.name, city5.name, city6.name
